0eface1a8f64e6179b094354da6f1d2a47646959
angel b <92060029+angelb215432@users.noreply.github.com>
2021-10-30 18:33:13 +0000

    cree el algoritmo para calcular el area de un cuarto rectangular

---
+++ area_de_un_cuarto_rectangular.py
@@ -0,0 +1,14 @@
+Inicio
+Definir ancho, largo, area_metros, area_pies, factor_conversion
+Hacer factor_conversion = 10.764
+Imprimir "Cual es el largo del cuarto en metros?"
+Hacer largo = entrada del usuario
+Imprimir "Cual es el ancho del cuarto en metros?"
+Hacer ancho = entrada del usuario
+Hacer area_metros = largo * ancho
+Hacer area_pies = factor_conversion * area_metros
+Imprimir "Has ingresado las sigientes dimensiones: ", ancho, "x", largo, "metros."
+Imprimir "El área es de: "
+Imprimir area_metros, "metros cuadrados."
+Imprimir area_pies "pies cuadrados."
+Fin
